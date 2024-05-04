ee511b03d1fe280d9d9e2e5a8b6727854c9729ef
Atiqur Rahman <atiqrs.me@gmail.com>
2024-05-04 19:30:56 +0600

    update home screen bff setup

--- lib/src/features/home_screen/data/datasources/home_rest_client.dart
+++ lib/src/features/home_screen/data/datasources/home_rest_client.dart
@@ -1,7 +1,38 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:food_recipe_demo/src/common/remote_data/dio_response_handler.dart';
+import 'package:food_recipe_demo/src/common/remote_data/dio_result.dart';
+import 'package:food_recipe_demo/src/common/remote_data/http_constants.dart';
+import 'package:food_recipe_demo/src/common/resources/endpoints.dart';
+import 'package:food_recipe_demo/src/features/home_screen/data/models/recipes_list_response_model.dart';
 
 class HomeRestClient {
   final Dio dio;
 
   HomeRestClient(this.dio);
+
+  Future<DioResult<RecipesListResponseModel>> getRecipeList({required String keyword}) async =>
+      await DioJsonResponseHandler<RecipesListResponseModel>().handleResponse(
+        requestAction: () async {
+          debugPrint("Recipe List rest client");
+          debugPrint("Recipe List endpoint: ${Endpoints.recipeListQuery(keyword)}");
+          debugPrint('''Recipe List header data: {"id": $keyword}''');
+
+          final originalOptions = dio.options;
+          final optionsWithCustomHeaders = dio.options.copyWith(
+              headers: originalOptions.headers
+                ..addAll({
+                  Header.rapidKey: Endpoints.rapidApiKey,
+                }));
+
+          dio.options = optionsWithCustomHeaders;
+          final result = dio.get<Map<String, dynamic>>(Endpoints.recipeListQuery(keyword));
+          dio.options = originalOptions;
+          return result;
+        },
+        mapJsonAction: (responseData) {
+          debugPrint("Recipe List responseData: $responseData");
+          return RecipesListResponseModel.fromJson(responseData);
+        },
+      );
 }
