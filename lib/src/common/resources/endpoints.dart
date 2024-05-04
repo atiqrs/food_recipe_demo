e9553a500fdcc3f6b0e107135f5f5ab432b7fc89
Atiqur Rahman <atiqrs.me@gmail.com>
2024-05-04 19:30:16 +0600

    update endpoints

--- lib/src/common/resources/endpoints.dart
+++ lib/src/common/resources/endpoints.dart
@@ -1,12 +1,15 @@
 class Endpoints {
   Endpoints._();
 
-  static const String baseUrl = "";
+  static const String rapidApiKey = "1a2bd7d529msh513ba3b70195a79p1acf77jsned01ab52cffa";
 
-  static const String home = "$baseUrl/home";
+  static const String baseUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com";
 
-  static const String recipeSearch = "$baseUrl/recipe";
-  static const String recipeFind = "$baseUrl/recipe/details";
+  static const String recipes = "$baseUrl/recipes";
 
-  static String recipeDetails(String id) => "$recipeFind/$id";
+  static const String recipeSearch = "$recipes/complexSearch";
+  static const String recipeFindPostfix = "/information";
+
+  static String recipeListQuery(String keyword) => "$recipeSearch?query=$keyword";
+  static String recipeDetails(String id) => "$recipes/$id$recipeFindPostfix";
 }
